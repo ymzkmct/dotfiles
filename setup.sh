a4be664bcee0857fc22b247fa36356be204c5751
ymzkmct <macoto.yamazaki@gmail.com>
2014-01-11 23:58:26 +0900

    ignore .git

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ dotfiles=`ls -A`
 cd ~
 for dotfile in $dotfiles
 do
-  if [ "setup.sh" != "$dotfile" ]; then
+  if [ "setup.sh" != "$dotfile" -a ".git" != "$dotfile" ]; then
     rm -f "./$dotfile"
     ln -s "$current/$dotfile" "./$dotfile"
   fi
